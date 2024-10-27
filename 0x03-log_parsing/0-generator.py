ece00177183939c64bfc2cfd93e06b3e16b25caf
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2024-10-27 23:02:00 +0300

    updates

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -4,7 +4,7 @@ import sys
 from time import sleep
 import datetime
 
-for i in range(1000):
+for i in range(10000):
     sleep(random.random())
     sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
         random.randint(1, 255), random.randint(
@@ -13,4 +13,3 @@ for i in range(1000):
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
-    sys.stdout.flush()
