1863133447058c4ce5d6d9c7338627ed82718732
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2024-10-27 22:55:57 +0300

    updates

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,57 @@
 #!/usr/bin/python3
-""" A script that reads stdin line by line and computes metrics """
-
+"""
+Read stdin line by line and compute metric
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>, skip line if not this format
+After every 10minutes or keyboard interrupt (CTRL + C)
+print these from beginning: number of lines by status code
+possible status codes: 200, 301, 400, 401, 404, 405, and 500
+if status code isn't an integer, do not print it
+format: <status code>: <number>
+Status code must be printed in ascending order
+"""
 import sys
 
 
-def print_status(dict, size):
-    """Print the format"""
-    print("File size: {}".format(size))
-    for key in sorted(dict.keys()):
-        if dict[key] != 0:
-            print("{}: {}".format(key, dict[key]))
+def print_msg(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-
-status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-               '404': 0, '405': 0, '500': 0}
 
 file_size = 0
-count = 0
+code = 0
+count_lines = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            print_status(status_dict, file_size)
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-        el = line.split(" ")
-        count += 1
+        if len(parsed_line) > 2:
+            count_lines += 1
 
-        try:
-            file_size += int(el[-1])
-        except:
-            pass
+            if count_lines <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-        try:
-            if el[-2] in status_dict.keys():
-                status_dict[el[-2]] += 1
-        except:
-            pass
-    print_status(status_dict, file_size)
+                if (code in codes.keys()):
+                    codes[code] += 1
 
+            if (count_lines == 10):
+                print_msg(codes, file_size)
+                count_lines = 0
 
-except KeyboardInterrupt:
-    print_status(status_dict, file_size)
-    raise
+finally:
+    print_msg(codes, file_size)
